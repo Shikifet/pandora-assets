49760b634ca956c3f5f656c9c79c7e7096b6616f
Jomshir98 <jomshir98@protonmail.com>
2023-05-06 16:15:41 +0000

    [DEV] Reorganize TS config files (#91)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
-/*
+/**
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
+ * @type { import('ts-jest').JestConfigWithTsJest }
  */
 module.exports = {
 	testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/', '<rootDir>/pandora/'],
@@ -9,6 +10,9 @@ module.exports = {
 	coverageDirectory: 'coverage',
 	errorOnDeprecated: true,
 	transform: {
-		'^.+\\.(t|j)sx?$': ['@swc/jest'],
+		'^.+\\.tsx?$': ['ts-jest', {
+			tsconfig: './test/tsconfig.json',
+			isolatedModules: true,
+		}],
 	},
 };
