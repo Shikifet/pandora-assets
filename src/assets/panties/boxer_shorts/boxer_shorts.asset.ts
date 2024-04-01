835a5484e61841952fd8f72f815bee6e07043434
Kane <59668834+Kane-678@users.noreply.github.com>
2024-04-01 05:24:49 -0500

    [ADD] boxer shorts (#214)

---
+++ src/assets/panties/boxer_shorts/boxer_shorts.asset.ts
@@ -0,0 +1,38 @@
+DefineAsset({
+	name: 'Boxer Shorts',
+	size: 'medium',
+	graphics: 'graphics.json',
+	colorization: {
+		waistBand: {
+			name: 'Waistband',
+			default: '#CDCDCD',
+		},
+		shorts: {
+			name: 'Shorts',
+			default: '#ECECEC',
+		},
+	},
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Clothing',
+			'Underwear',
+			'Underwear_panties',
+		],
+		hides: ['Penis'],
+	},
+	ownership: {
+		responsibleContributor: 'Kane-678 <59668834+Kane-678@users.noreply.github.com>',
+		credits: ['Kane'],
+		modificationPolicy: 'Fixes only, otherwise ask',
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'Kane',
+				editedBy: 'Kane',
+				license: 'Pandora-Use-Only',
+			},
+		],
+	},
+});
