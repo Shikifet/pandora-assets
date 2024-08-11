838b7d99d5ae708a93331ebc2da95721a2adc39e
Jomshir98 <jomshir98@protonmail.com>
2024-08-11 12:49:16 +0200

    [DEV] Switch pandora-common from submodule to be pulled by pnpm (#263)

--- .hooks/include.sh
+++ .hooks/include.sh
@@ -18,5 +18,11 @@ function submodule_update() {
 	if [ "${SKIP_SUBMODULE_UPDATE:-}" == "true" ]; then
 		return
 	fi
+	# Cleanup old submodule
+	if [ -d ".git/modules/pandora" ]; then
+		git config -f .git/config --remove-section "submodule.pandora"
+		rm -rf .git/modules/pandora
+		git clean -Xdf pandora
+	fi
 	git submodule update --init --recursive
 }
