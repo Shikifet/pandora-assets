6c75258996eb7e463edb2b3d22f05d9d2e79ad3e
Sandrine <118102950+SandrinePDR@users.noreply.github.com>
2024-08-19 18:38:05 +0200

    [ADD] Second way to wear the ponytail (#265)

--- src/assets/body/back_hair_ponytail/back_hair_ponytail.asset.ts
+++ src/assets/body/back_hair_ponytail/back_hair_ponytail.asset.ts
@@ -18,17 +18,34 @@ DefineAsset({
 			'Hair_back',
 		],
 	},
-	modules,
+	modules: {
+		...modules,
+		direction: {
+			type: 'typed',
+			name: 'Ponytail direction',
+			variants: [
+				{
+					id: 'left',
+					name: 'Left',
+					default: true,
+				},
+				{
+					id: 'right',
+					name: 'Right',
+				},
+			],
+		},
+	},
 	ownership: {
 		responsibleContributor: 'Jomshir98 <jomshir98@protonmail.com>',
-		credits: ['Titania', 'Jomshir', 'ClaudiaMia'],
+		credits: ['Titania', 'Jomshir', 'ClaudiaMia', 'SandrinePDR'],
 		modificationPolicy: `Fixes and New uses, otherwise ask`,
 		reusePolicy: 'Ask first',
 		licensing: [
 			{
 				source: 'Private',
 				copyrightHolder: 'QueenTita',
-				editedBy: 'ClaudiaMia',
+				editedBy: 'ClaudiaMia & SandrinePDR',
 				license: 'Pandora-Use-Only-v1-or-later',
 			},
 		],
