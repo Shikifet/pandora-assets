16e888619741d3d13f539d37fb9ceac3bae7109e
Sandrine <118102950+SandrinePDR@users.noreply.github.com>
2024-09-04 16:07:05 +0200

    [ADD] New Asset: Leggings (#276)
    Co-authored-by: ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>

---
+++ src/assets/bottoms/leggings/leggings.asset.ts
@@ -0,0 +1,51 @@
+DefineAsset({
+	name: 'Leggings',
+	size: 'small',
+	graphics: 'graphics.json',
+	colorization: {
+		leggings: {
+			name: 'Leggings',
+			default: '#2A2A2A',
+		},
+	},
+	// size:800, y:574, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Clothing',
+			'Clothing_lower',
+		],
+	},
+	modules: {
+		design: {
+			type: 'typed',
+			name: 'Leggings design',
+			variants: [
+				{
+					id: 'netshowing',
+					name: 'Net showing',
+					default: true,
+				},
+				{
+					id: 'plain',
+					name: 'plain',
+				},
+			],
+		},
+	},
+	ownership: {
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['SandrinePDR'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				part: 'leggings',
+				source: 'Self-Made',
+				copyrightHolder: 'ClaudiaMia',
+				editedBy: 'SandrinePDR',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
