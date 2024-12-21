a6ba95d4afe641ea3f8f5a316325600ccaa0415c
Sandrine <118102950+SandrinePDR@users.noreply.github.com>
2024-12-21 07:50:08 +0100

    [CHANGE] Split layers for present room device (#326)

--- src/assets/room_devices/presents/presents.asset.ts
+++ src/assets/room_devices/presents/presents.asset.ts
@@ -1,6 +1,21 @@
 DefineRoomDeviceAsset({
 	name: 'Christmas Presents',
 	size: 'huge',
+	colorization: {
+		color_1: {
+			name: 'Color 1',
+			default: '#FFFFFF',
+		},
+		color_2: {
+			name: 'Color 2',
+			default: '#33922C',
+		},
+		color_3: {
+			name: 'Color 3',
+			default: '#EF5151',
+		},
+	},
+
 	staticAttributes: ['Floor'],
 	preview: 'preview.png',
 	slots: {},
@@ -9,18 +24,29 @@ DefineRoomDeviceAsset({
 			type: 'storage',
 			name: 'Gifts',
 			staticConfig: { slotName: null },
-			maxCount: 3,
+			maxCount: 7,
 			maxAcceptedSize: 'large',
 		},
 	},
 	pivot: {
-		x: 350,
+		x: 660,
 		y: 400,
 	},
 	graphicsLayers: [
 		{
 			type: 'sprite',
-			image: 'presents.png',
+			image: 'presents_color_1.png',
+			colorizationKey: 'color_1',
+		},
+		{
+			type: 'sprite',
+			image: 'presents_color_2.png',
+			colorizationKey: 'color_2',
+		},
+		{
+			type: 'sprite',
+			image: 'presents_color_3.png',
+			colorizationKey: 'color_3',
 		},
 	],
 	ownership: {
@@ -39,7 +65,7 @@ DefineRoomDeviceAsset({
 			{
 				part: 'Christmas present base image',
 				source: 'https://sketchfab.com/3d-models/game-ready-free-christmas-gifts-ad99e7faf07c45d9b57324b4a0e94c53',
-				copyrightHolder: 'Saritasa: https://creativecommons.org/licenses/by/4.0/',
+				copyrightHolder: 'Saritasa',
 				editedBy: 'Sandrine',
 				license: 'CC BY',
 			},
