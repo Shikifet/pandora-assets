565ff90d73507167f68d4820872500aef5848c74
Shikifet <shikifet@gmail.com>
2025-01-15 19:42:01 +0100

    [CHANGE] Add thighs graphics

--- src/assets/foot_restraints/leg_jute_ropes/leg_jute_ropes.asset.ts
+++ src/assets/foot_restraints/leg_jute_ropes/leg_jute_ropes.asset.ts
@@ -6,7 +6,7 @@ DefineAsset({
 	colorization: {
 		rope: {
 			name: 'Rope',
-			default: '#C79A32',
+			default: '#D7AC4D',
 		},
 	},
 	// size:415, y:940, centered
@@ -18,53 +18,245 @@ DefineAsset({
 		],
 
 	},
-	poseLimits: {
-		options: [
-			{
-				legs: 'standing',
-				bones: {
-					leg_r: 2,
-					leg_l: 2,
-				},
-			},
-			{
-				legs: 'sitting',
-				bones: {
-					leg_r: 2,
-					leg_l: 2,
-				},
-			},
-			{
-				legs: 'kneeling',
-				bones: {
-					leg_r: 5,
-					leg_l: 5,
-				},
-			},
-		],
-	},
 	modules: {
-		legs: {
+		thighs: {
 			type: 'typed',
-			name: 'Legs',
+			name: 'Thighs',
 			variants: [
+				{
+					id: 'none',
+					name: 'None',
+				},
 				{
 					id: 'single',
 					name: 'Single',
+					properties: {
+						poseLimits: {
+							options: [
+								{
+									legs: 'standing',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'sitting',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'kneeling',
+									bones: {
+										leg_r: 5,
+										leg_l: 5,
+									},
+								},
+							],
+						}
+					},
 
 					default: true
 				},
 				{
 					id: 'double',
-					name: 'Double'
-				},
-				{
-					id: 'complex',
-					name: 'Complex'
+					name: 'Double',
+					properties: {
+						poseLimits: {
+							options: [
+								{
+									legs: 'standing',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'sitting',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'kneeling',
+									bones: {
+										leg_r: 5,
+										leg_l: 5,
+									},
+								},
+							],
+						}
+					},
 				},
 				{
 					id: 'full',
-					name: 'Full'
+					name: 'Full',
+					properties: {
+						poseLimits: {
+							options: [
+								{
+									legs: 'standing',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'sitting',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'kneeling',
+									bones: {
+										leg_r: 5,
+										leg_l: 5,
+									},
+								},
+							],
+						}
+					},
+				}
+			]
+		},
+		legs: {
+			type: 'typed',
+			name: 'Legs',
+			variants: [
+				{
+					id: 'none',
+					name: 'None',
+				},
+				{
+					id: 'single',
+					name: 'Single',
+					properties: {
+						poseLimits: {
+							options: [
+								{
+									legs: 'standing',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'sitting',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'kneeling',
+									bones: {
+										leg_r: 5,
+										leg_l: 5,
+									},
+								},
+							],
+						}
+					},
+
+					default: true
+				},
+				{
+					id: 'double',
+					name: 'Double',
+					properties: {
+						poseLimits: {
+							options: [
+								{
+									legs: 'standing',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'sitting',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'kneeling',
+									bones: {
+										leg_r: 5,
+										leg_l: 5,
+									},
+								},
+							],
+						}
+					},
+				},
+				{
+					id: 'complex',
+					name: 'Complex',
+					properties: {
+						poseLimits: {
+							options: [
+								{
+									legs: 'standing',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'sitting',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'kneeling',
+									bones: {
+										leg_r: 5,
+										leg_l: 5,
+									},
+								},
+							],
+						}
+					},
+				},
+				{
+					id: 'full',
+					name: 'Full',
+					properties: {
+						poseLimits: {
+							options: [
+								{
+									legs: 'standing',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'sitting',
+									bones: {
+										leg_r: 2,
+										leg_l: 2,
+									},
+								},
+								{
+									legs: 'kneeling',
+									bones: {
+										leg_r: 5,
+										leg_l: 5,
+									},
+								},
+							],
+						}
+					},
 				}
 			]
 		},
