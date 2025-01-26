fd46214ae97941618b094974e3def3525b8ece81
Sandrine <118102950+SandrinePDR@users.noreply.github.com>
2025-01-26 18:20:02 +0100

    [ADD] Gloves (#309)
    Co-authored-by: ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>
    Co-authored-by: jomshir98 <jomshir98@protonmail.com>

---
+++ src/assets/gloves/gloves/gloves.asset.ts
@@ -0,0 +1,79 @@
+DefineAsset({
+	name: 'Gloves',
+	size: 'small',
+	graphics: 'graphics.json',
+	colorization: {
+		gloves: {
+			name: 'Gloves',
+			default: '#FFFFFF',
+			minAlpha: 0.3,
+		},
+		shine: {
+			name: 'Shine',
+			default: '#FFFFFF00',
+			minAlpha: 0,
+		},
+		pattern: {
+			name: 'Pattern',
+			default: '#202020',
+		},
+	},
+	// size: 300, y: 380, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Gloves',
+		],
+	},
+	modules: {
+		length: {
+			type: 'typed',
+			name: 'Glove length',
+			variants: [
+				{
+					id: 'long',
+					name: 'Opera gloves',
+					default: true,
+				},
+				{
+					id: 'medium',
+					name: 'Regular gloves',
+				},
+				{
+					id: 'short',
+					name: 'Short gloves',
+				},
+			],
+		},
+		pattern: {
+			type: 'typed',
+			name: 'Pattern',
+			variants: [
+				{
+					id: 'none',
+					name: 'None',
+					default: true,
+				},
+				{
+					id: 'cow',
+					name: 'Cow print',
+				},
+			],
+		},
+	},
+	ownership: {
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['Taja'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				part: 'gloves',
+				source: 'Self-Made',
+				copyrightHolder: 'Taja',
+				editedBy: 'Sandrine',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
