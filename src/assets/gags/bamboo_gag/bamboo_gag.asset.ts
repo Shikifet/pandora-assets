5e20d4d2fff38d9fb81cfd61f55e4c7374ed0d58
Shikifet <shikifet@gmail.com>
2025-02-08 22:20:46 +0100

    [ADD] Rope and Bamboo gags (#360)
    Co-authored-by: Jomshir98 <jomshir98@protonmail.com>

---
+++ src/assets/gags/bamboo_gag/bamboo_gag.asset.ts
@@ -0,0 +1,62 @@
+DefineAsset({
+	name: 'Bamboo Gag',
+	size: 'small',
+	requireFreeHandsToUseDefault: true,
+	graphics: 'graphics.json',
+	colorization: {
+		bamboo: {
+			name: 'Bamboo',
+			default: '#EFCA7B',
+		},
+		rope: {
+			name: 'Rope',
+			default: '#D7AC4D',
+		},
+	},
+	// size:200, y:197, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Restraint',
+			'Restraint_mouth',
+			'Mouth_item',
+			'Mouth_insert',
+			'Mouth_cover',
+		],
+		requires: [
+			'Mouth_open_wide',
+			'!Mouth_protruding',
+			'!Mouth_cover',
+		],
+		covers: [
+			'Mouth_item',
+		],
+	},
+	effects: {
+		lipsTouch: 6,
+		jawMove: 3,
+		tongueRoof: 2,
+		mouthBreath: 2,
+		throatBreath: 1,
+		coherency: 2,
+		stimulus: 1,
+	},
+	chat: {
+		actionAdd: 'SOURCE_CHARACTER tied a Bamboo Gag tightly between TARGET_CHARACTER_DYNAMIC_POSSESSIVE teeth.',
+		actionRemove: 'SOURCE_CHARACTER loosened and then removed the Bamboo Gag from TARGET_CHARACTER_DYNAMIC_POSSESSIVE mouth.',
+	},
+	ownership: {
+		responsibleContributor: 'Shikifet <shikifet@gmail.com>',
+		credits: ['Shikifet'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'Shikifet',
+				editedBy: 'Shikifet',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
---
+++ src/assets/gags/jute_rope_gag/jute_rope_gag.asset.ts
@@ -0,0 +1,113 @@
+DefineAsset({
+	name: 'Jute Rope Gag',
+	size: 'small',
+	requireFreeHandsToUseDefault: true,
+	graphics: 'graphics.json',
+	colorization: {
+		rope: {
+			name: 'Rope',
+			default: '#D7AC4D',
+		},
+	},
+	// size:200, y:197, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Restraint',
+			'Restraint_mouth',
+			'Mouth_item',
+			'Mouth_insert',
+			'Mouth_cover',
+		],
+		requires: [
+			'Mouth_open_wide',
+			'!Mouth_protruding',
+			'!Mouth_cover',
+		],
+		covers: [
+			'Mouth_item',
+		],
+	},
+	modules: {
+		gagType: {
+			type: 'typed',
+			name: 'Gag Type',
+			variants: [
+				{
+					id: 'rope',
+					name: 'Rope',
+					properties: {
+						effects: {
+							lipsTouch: 2,
+							jawMove: 3,
+							tongueRoof: 3,
+							mouthBreath: 4,
+							throatBreath: 2,
+							coherency: 2,
+							stimulus: 1,
+						},
+					},
+				},
+				{
+					id: 'knot',
+					name: 'Knot',
+					properties: {
+						attributes: {
+							requires: [
+								'!Mouth_tongue_out',
+							],
+						},
+						effects: {
+							lipsTouch: 7,
+							jawMove: 8,
+							tongueRoof: 6,
+							mouthBreath: 3,
+							throatBreath: 2,
+							coherency: 4,
+							stimulus: 2,
+						},
+					},
+				},
+				{
+					id: 'bit',
+					name: 'Bit',
+					default: true,
+					properties: {
+						attributes: {
+							requires: [
+								'!Mouth_tongue_out',
+							],
+						},
+						effects: {
+							lipsTouch: 8,
+							jawMove: 9,
+							tongueRoof: 7,
+							mouthBreath: 4,
+							throatBreath: 3,
+							coherency: 5,
+							stimulus: 2,
+						},
+					},
+				},
+			],
+		},
+	},
+	chat: {
+		actionAdd: 'SOURCE_CHARACTER tied a Rope Gag tightly between TARGET_CHARACTER_DYNAMIC_POSSESSIVE teeth.',
+		actionRemove: 'SOURCE_CHARACTER loosened and then removed the Rope Gag from TARGET_CHARACTER_DYNAMIC_POSSESSIVE mouth.',
+	},
+	ownership: {
+		responsibleContributor: 'Shikifet <shikifet@gmail.com>',
+		credits: ['Shikifet'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'Shikifet',
+				editedBy: 'Shikifet',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
