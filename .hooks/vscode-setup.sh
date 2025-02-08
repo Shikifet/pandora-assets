cc84ff66ece13629de1fad142aa138d1d13bd707
Jomshir98 <jomshir98@protonmail.com>
2025-02-08 20:49:50 +0100

    [FIX] Update corepack npm registry keys (#363)

--- .hooks/vscode-setup.sh
+++ .hooks/vscode-setup.sh
@@ -17,6 +17,15 @@ else
 	echo
 	echo "Running pnpm install..."
 
+	# Update npm keys (ignoring any failures)
+	local npm_keys="$(curl -s https://registry.npmjs.org/-/npm/v1/keys | jq -c '{npm: .keys}' || echo '')"
+	if [[ "$npm_keys" ]]; then
+		export COREPACK_INTEGRITY_KEYS="$npm_keys"
+	fi
+
+	# Do not prompt for user confirmation if downloading new pnpm version
+	export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
+
 	pnpm install --frozen-lockfile --prefer-offline
 fi
 
