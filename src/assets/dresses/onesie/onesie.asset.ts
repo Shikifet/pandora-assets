b4bea54f36ca46f90641e033100c3a3b94f65add
Sandrine <118102950+SandrinePDR@users.noreply.github.com>
2025-02-13 07:48:51 +0100

    [ADD] Onesie (#368)

---
+++ src/assets/dresses/onesie/onesie.asset.ts
@@ -0,0 +1,92 @@
+DefineAsset({
+	name: 'Onesie',
+	size: 'large',
+	allowRandomizerUsage: false,
+	graphics: 'graphics.json',
+	colorization: {
+		onesie: {
+			name: 'Onesie',
+			default: '#EEEEEE',
+		},
+		pockets: {
+			name: 'Pockets',
+			default: '#FFEBB6',
+		},
+		applications: {
+			name: 'Applications',
+			default: '#FFEBB6',
+		},
+		buttons: {
+			name: 'Buttons',
+			default: '#FFEBB6',
+		},
+	},
+	// size:450, y:380, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Clothing',
+			'Clothing_upper',
+			'Clothing_lower',
+			'Clothing_large',
+			'Vulva_cover',
+		],
+		hides: [
+			'Penis',
+		],
+	},
+	modules: {
+		flap: {
+			type: 'typed',
+			name: 'Flap',
+			variants: [
+				{
+					id: 'closed',
+					name: 'Closed',
+					default: true,
+					properties: {
+						attributes: {
+							provides: [
+								'Anus_cover',
+							],
+						},
+					},
+				},
+				{
+					id: 'open',
+					name: 'Open',
+				},
+			],
+		},
+		pattern: {
+			type: 'typed',
+			name: 'Pattern',
+			variants: [
+				{
+					id: 'none',
+					name: 'No Pattern',
+					default: true,
+				},
+				{
+					id: 'toys',
+					name: 'Baby Toys',
+				},
+			],
+		},
+	},
+	ownership: {
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['Taja'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				part: 'onesie',
+				source: 'Self-Made',
+				copyrightHolder: 'Taja',
+				editedBy: 'Sandrine',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
