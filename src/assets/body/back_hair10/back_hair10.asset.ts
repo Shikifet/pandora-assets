7668839b2fdfc76d617c696b632b1991fd36017b
Sandrine <118102950+SandrinePDR@users.noreply.github.com>
2025-02-16 11:35:58 +0100

    [ADD] New hairstyles (#364)

---
+++ src/assets/body/back_hair10/back_hair10.asset.ts
@@ -0,0 +1,34 @@
+import { CreateHairColor } from '../../../helpers/hair_base.js';
+const { colorization, modules } = CreateHairColor(true);
+
+DefineBodypart({
+	name: 'Back hair 10',
+	bodypart: 'backhair',
+	allowRandomizerUsage: true,
+	graphics: 'graphics.json',
+	colorization,
+	// size:280, y:176, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Hair',
+			'Hair_back',
+		],
+	},
+	modules,
+	ownership: {
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['Taja'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				part: 'hairstyle',
+				source: 'Self-Made',
+				copyrightHolder: 'Taja',
+				editedBy: 'Sandrine',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
