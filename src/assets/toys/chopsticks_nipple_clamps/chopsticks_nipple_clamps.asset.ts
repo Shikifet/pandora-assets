70b0c7df090b5858ca5a16c28415908866681871
Shikifet <shikifet@gmail.com>
2025-02-18 11:38:20 +0100

    [ADD] Create Chopsticks Nipple clamps (wip only right nipple)

---
+++ src/assets/toys/chopsticks_nipple_clamps/chopsticks_nipple_clamps.asset.ts
@@ -0,0 +1,80 @@
+DefineAsset({
+	name: 'Chopsticks Nipple Clamps',
+	size: 'small',
+	requireFreeHandsToUseDefault: true,
+	graphics: 'graphics.json',
+	colorization: {
+		chopsticks: {
+			name: 'Chopsticks',
+			default: '#EFCA7B',
+		},
+		strings: {
+			name: 'Strings',
+			default: '#8B5E00',
+		},
+	},
+	// size:200, y:197, centered
+	preview: null,
+	attributes: {
+		provides: [
+			'Toy',
+			'Breast_item',
+			'Breast_nipple',
+		],
+	},
+	modules: {
+		attach: {
+			type: 'typed',
+			name: 'Attach on',
+			variants: [
+				{
+					id: 'both',
+					name: 'Both',
+					default: true,
+				},
+				{
+					id: 'left',
+					name: 'Left Only',
+				},
+				{
+					id: 'right',
+					name: 'Right Only',
+				},
+			],
+		},
+		position: {
+			type: 'typed',
+			name: 'Position',
+			variants: [
+				{
+					id: 'vertical',
+					name: 'Vertical',
+					default: true,
+				},
+				{
+					id: 'horizontal',
+					name: 'Horizontal',
+					default: true,
+				},
+			],
+		},
+	},
+	chat: {
+		actionAdd: 'SOURCE_CHARACTER attached Chopsticks Nipple Clamps to both of TARGET_CHARACTER_DYNAMIC_POSSESSIVE nipples.',
+		actionRemove: 'SOURCE_CHARACTER removed the Chopsticks Nipple Clamps from TARGET_CHARACTER_DYNAMIC_POSSESSIVE nipples.',
+	},
+	ownership: {
+		responsibleContributor: 'Shikifet <shikifet@gmail.com>',
+		credits: ['Shikifet'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'Shikifet',
+				editedBy: 'Shikifet',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
