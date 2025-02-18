157bbbd3d30110ad74a240a620174490db05a2bc
Sandrine <118102950+SandrinePDR@users.noreply.github.com>
2025-02-18 20:23:03 +0100

    [ADD] New underwear (#377)

--- src/assets/panties/slip/slip.asset.ts
+++ src/assets/panties/slip/slip.asset.ts
@@ -1,5 +1,5 @@
 DefineAsset({
-	name: 'Plain Panties',
+	name: 'Slip',
 	size: 'small',
 	allowRandomizerUsage: true,
 	graphics: 'graphics.json',
@@ -41,16 +41,6 @@ DefineAsset({
 				{
 					id: 'aside',
 					name: 'Pulled Aside',
-				},
-				{
-					id: 'wedged',
-					name: 'Wedged Up',
-					properties: {
-						attributes: {
-							provides: ['Vulva_cover'],
-							requires: ['!Penis'],
-						},
-					},
 				},
 				{
 					id: 'knees',
@@ -95,15 +85,15 @@ DefineAsset({
 		},
 	},
 	ownership: {
-		responsibleContributor: 'ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>',
-		credits: ['ClaudiaMia', 'Jomshir', 'Sandrine'],
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['Taja'],
 		modificationPolicy: `Fixes and New uses, otherwise ask`,
 		reusePolicy: 'Ask first',
 		licensing: [
 			{
 				source: 'Self-Made',
-				copyrightHolder: 'ClaudiaMia',
-				editedBy: 'ClaudiaMia',
+				copyrightHolder: 'Taja',
+				editedBy: 'Sandrine',
 				license: 'Pandora-Use-Only-v1-or-later',
 			},
 		],
