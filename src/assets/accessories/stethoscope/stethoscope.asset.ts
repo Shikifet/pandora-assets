7054f8a5eba95caad0279ba04fe75fd319ee060c
Sandrine <118102950+SandrinePDR@users.noreply.github.com>
2025-04-06 10:21:37 +0200

    [ADD] Stethoscope (#417)

---
+++ src/assets/accessories/stethoscope/stethoscope.asset.ts
@@ -0,0 +1,27 @@
+DefineAsset({
+	name: 'Stethoscope',
+	size: 'small',
+	graphics: 'graphics.json',
+	// size:250, y:350, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Clothing',
+			'Accessory',
+		],
+	},
+	ownership: {
+		responsibleContributor: 'Sandrine <118102950+SandrinePDR@users.noreply.github.com>',
+		credits: ['Taja'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'Taja',
+				editedBy: 'Sandrine',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
