07a2499053f8427751e8a5c9c1b32540d9f8ff91
Freyja <247626786+freyja-veritas@users.noreply.github.com>
2025-12-07 23:44:08 +0200

    [ADD] New TOP -> 'Tank Top' with cropped & cropped more variants (#534)
    Co-authored-by: Jomshir98 <jomshir98@protonmail.com>

---
+++ src/assets/tops/tank-top/tank-top.asset.ts
@@ -0,0 +1,57 @@
+DefineAsset({
+	name: 'Tank Top',
+	size: 'medium',
+	allowRandomizerUsage: true,
+	graphics: 'graphics.json',
+	colorization: {
+		tank_top: {
+			name: 'Tank Top',
+			default: '#6E8991',
+		},
+	},
+	// size:350, y:380, centered
+	preview: 'preview.png',
+	attributes: {
+		provides: [
+			'Clothing',
+			'Clothing_upper',
+		],
+	},
+	modules: {
+		type: {
+			type: 'typed',
+			name: 'Tank Top Type',
+			variants: [
+				{
+					id: 'regular',
+					name: 'Regular',
+					default: true,
+				},
+				{
+					id: 'cropped',
+					name: 'Cropped',
+				},
+				{
+					id: 'cropped_more',
+					name: 'Cropped More',
+				},
+			],
+		},
+	},
+	ownership: {
+		responsibleContributor: 'Freyja <247626786+freyja-veritas@users.noreply.github.com>',
+		credits: ['Freyja'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				part: 'tank top',
+				source: 'Self-Made',
+				copyrightHolder: 'Freyja',
+				editedBy: 'Freyja',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
+
