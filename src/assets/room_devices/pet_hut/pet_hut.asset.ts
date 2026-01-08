17a93a5c812bbe7881c373282242feb67e9d1a6a
Sandrine <118102950+SandrinePDR@users.noreply.github.com>
2026-01-08 20:33:25 +0100

    [ADD] Door to the pet hut (#559)

--- src/assets/room_devices/pet_hut/pet_hut.asset.ts
+++ src/assets/room_devices/pet_hut/pet_hut.asset.ts
@@ -14,7 +14,18 @@ DefineRoomDeviceAsset({
 			name: 'Cushion',
 			default: '#89BEF0',
 		},
-
+		doorFrame: {
+			name: 'Door frame',
+			default: '#ffffff',
+		},
+		doorBars: {
+			name: 'Metal bars',
+			default: '#ffffff',
+		},
+		doorRivets: {
+			name: 'Rivets',
+			default: '#ffffff',
+		},
 	},
 	staticAttributes: ['Furniture'],
 	preview: 'preview.png',
@@ -73,6 +84,87 @@ DefineRoomDeviceAsset({
 				},
 			],
 		},
+		door: {
+			type: 'typed',
+			name: 'Hut door',
+			staticConfig: { slotName: null },
+			variants: [
+				{
+					id: 'doorNo',
+					name: 'No Door',
+					default: true,
+				},
+				{
+					id: 'doorOpen',
+					name: 'Open',
+				},
+				{
+					id: 'doorClosed',
+					name: 'Closed',
+					properties: {
+						blockSlotsEnterLeave: [
+							'character_slot_kneeling',
+							'character_slot_inside',
+						],
+					},
+
+				},
+			],
+		},
+		anchor_rear: {
+			type: 'typed',
+			name: 'Lying spot',
+			staticConfig: { slotName: 'character_slot_inside' },
+			variants: [
+				{
+					id: 'free_rear',
+					name: 'Free',
+					default: true,
+				},
+				{
+					id: 'tied_rear',
+					name: 'Anchored to the hut',
+					properties: {
+						blockSlotsEnterLeave: ['character_slot_inside'],
+					},
+				},
+			],
+		},
+		lock_rear: {
+			type: 'lockSlot',
+			name: 'Lying spot anchor lock',
+			staticConfig: { slotName: 'character_slot_inside' },
+			lockedProperties: {
+				blockModules: ['anchor_rear'],
+			},
+		},
+		anchor_front: {
+			type: 'typed',
+			name: 'Kneeling spot',
+			staticConfig: { slotName: 'character_slot_kneeling' },
+			variants: [
+				{
+					id: 'free_front',
+					name: 'Free',
+					default: true,
+				},
+				{
+					id: 'tied_front',
+					name: 'Anchored to the hut',
+					properties: {
+						blockSlotsEnterLeave: ['character_slot_kneeling'],
+					},
+				},
+			],
+		},
+		lock_front: {
+			type: 'lockSlot',
+			name: 'Kneeling spot anchor lock',
+			staticConfig: { slotName: 'character_slot_kneeling' },
+			lockedProperties: {
+				blockModules: ['anchor_front'],
+			},
+		},
 		storage: {
 			type: 'storage',
 			name: `Hut's floor`,
@@ -89,7 +181,7 @@ DefineRoomDeviceAsset({
 	graphics: 'roomDeviceGraphics.json',
 	ownership: {
 		responsibleContributor: 'ClaudiaMia <99583892+ClaudiaMia@users.noreply.github.com>',
-		credits: ['ClaudiaMia'],
+		credits: ['ClaudiaMia', 'Sandrine'],
 		modificationPolicy: `Fixes and New uses, otherwise ask`,
 		reusePolicy: 'Ask first',
 		licensing: [
@@ -99,6 +191,12 @@ DefineRoomDeviceAsset({
 				copyrightHolder: 'newmag2207',
 				editedBy: 'ClaudiaMia',
 				license: 'CC BY',
+			},
+			{
+				part: 'Door',
+				source: 'Self-Made',
+				copyrightHolder: 'Sandrine',
+				license: 'Pandora-Use-Only-v1-or-later',
 			},
 		],
 	},
