348fa541db8d68bc459ea54cfbd5a45aabd076b2
ObliqueBC <obliquebc@hotmail.com>
2026-01-20 06:01:50 +0100

    [ADD] paired front and back hair 13 (#572)

---
+++ src/assets/body/back_hair13/back_hair13.asset.ts
@@ -0,0 +1,59 @@
+import { CreateHairColor } from '../../../helpers/hair_base.ts';
+const { colorization, modules } = CreateHairColor(false);
+
+const bodypart = DefineBodypart({
+	name: 'Back Hair 13 (paired)',
+	bodypart: 'backhair',
+	graphics: 'graphics.json',
+	colorization,
+	preview: 'preview.png',
+	modules: {
+		...modules,
+		brushed_to: {
+			type: 'typed',
+			name: 'Brushed towards',
+			variants: [
+				{
+					id: 'left',
+					name: 'Left',
+					default: true,
+				},
+				{
+					id: 'right',
+					name: 'Right',
+				},
+			],
+		},
+	},
+	ownership: {
+		responsibleContributor: 'Oblique <obliquebc@hotmail.com>',
+		credits: ['Oblique'],
+		modificationPolicy: 'Fixes and New uses, otherwise ask',
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'Oblique',
+				editedBy: 'Oblique',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
+
+DefineAsset({
+	...bodypart,
+	id: 'body/back_hair13/wig',
+	name: 'Back Wig 13',
+	size: 'small',
+	attributes: {
+		provides: [
+			'Wig',
+			'Wig_front',
+		],
+		hides: [
+			'Hair_front',
+		],
+	},
+});
+
