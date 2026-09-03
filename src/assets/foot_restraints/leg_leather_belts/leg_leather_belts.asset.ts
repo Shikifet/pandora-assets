7f52904d83f3a088ad5c7fdb4c23a023ef6188e1
Shikifet <shikifet@gmail.com>
2026-09-03 18:36:37 +0200

    Add buckles and details to leg leather asset

---
+++ src/assets/foot_restraints/leg_leather_belts/leg_leather_belts.asset.ts
@@ -0,0 +1,184 @@
+import { CreateLeatherColor } from '../../../helpers/leather_base.ts';
+const { colorization: baseColorization, modules: baseModules } = CreateLeatherColor();
+
+DefineAsset({
+	name: 'Leg Leather Belts',
+	size: 'small',
+	graphics: 'graphics.json',
+	colorization: {
+		...baseColorization,
+		buckles: {
+			name: 'Buckles',
+			default: '#DADADA',
+		}
+	},
+	// size:450, y:377, centered
+	preview: null,
+	attributes: {
+		provides: [
+			'Restraint',
+			'Restraint_legs',
+		],
+	},
+	modules: {
+		...baseModules,
+		thighs: {
+			type: 'typed',
+			name: 'Thighs',
+			variants: [
+				{
+					id: 'no',
+					name: 'No',
+					default: true,
+				},
+				{
+					id: 'yes',
+					name: 'Yes',
+					properties: {
+						stateFlags: {
+							provides: ['belted_legs'],
+						},
+					},
+				},
+			],
+		},
+		knees: {
+			type: 'typed',
+			name: 'Knees',
+			variants: [
+				{
+					id: 'no',
+					name: 'No',
+					default: true,
+				},
+				{
+					id: 'above',
+					name: 'Above',
+					properties: {
+						stateFlags: {
+							provides: ['belted_legs'],
+						},
+					},
+				},
+				{
+					id: 'below',
+					name: 'Below',
+					properties: {
+						stateFlags: {
+							provides: ['belted_legs'],
+						},
+					},
+				},
+				{
+					id: 'both',
+					name: 'Both',
+					properties: {
+						stateFlags: {
+							provides: ['belted_legs'],
+						},
+					},
+				},
+			],
+		},
+		shins: {
+			type: 'typed',
+			name: 'Shins',
+			variants: [
+				{
+					id: 'no',
+					name: 'No',
+					default: true,
+				},
+				{
+					id: 'yes',
+					name: 'Yes',
+					properties: {
+						stateFlags: {
+							provides: ['belted_legs'],
+						},
+					},
+				},
+			],
+		},
+		ankles: {
+			type: 'typed',
+			name: 'Ankles',
+			variants: [
+				{
+					id: 'no',
+					name: 'No',
+					default: true,
+				},
+				{
+					id: 'yes',
+					name: 'Yes',
+					properties: {
+						stateFlags: {
+							provides: ['belted_legs'],
+						},
+					},
+				},
+			],
+		},
+		lock: {
+			type: 'lockSlot',
+			name: 'Lock',
+			lockedProperties: {
+				blockAddRemove: true,
+				blockModules: ['colorGroupLeather', 'thighs', 'knees', 'shins', 'ankles'],
+			},
+		},
+	},
+	stateFlagCombinations: [
+		{
+			requiredFlags: ['belted_legs'],
+			properties: {
+				poseLimits: {
+					options: [
+						{
+							legs: {
+								pose: 'standing',
+							},
+							bones: {
+								leg_r: 2,
+								leg_l: 2,
+							},
+						},
+						{
+							legs: {
+								pose: 'sitting',
+							},
+							bones: {
+								leg_r: 2,
+								leg_l: 2,
+							},
+						},
+						{
+							legs: {
+								pose: 'kneeling',
+							},
+							bones: {
+								leg_r: 5,
+								leg_l: 5,
+							},
+						},
+					],
+				},
+			},
+		},
+	],
+	ownership: {
+		responsibleContributor: 'Shikifet <shikifet@gmail.com>',
+		credits: ['Shikifet'],
+		modificationPolicy: `Fixes and New uses, otherwise ask`,
+		reusePolicy: 'Ask first',
+		licensing: [
+			{
+				source: 'Self-Made',
+				copyrightHolder: 'Shikifet',
+				editedBy: 'Shikifet',
+				license: 'Pandora-Use-Only-v1-or-later',
+			},
+		],
+	},
+});
